32baf195ead798ea6faf5c2e67380ef875eb68e3
wangbo <seetywangbo@gmail.com>
2020-05-25 21:50:45 +0800

    new

--- Promise/demo.js
+++ Promise/demo.js
@@ -1,15 +1,43 @@
 const fs = require('fs')
 
-let promise = new Promise((reject, resolve) => {
-    fs.readFile('./data.txt', 'utf8', (err, data) => {
+let p1 = new Promise((reject, resolve) => {
+    fs.readFile('./a.txt', 'utf8', (err, data) => {
+        if(err)
+            reject(err)
+        resolve(data)
+    })
+})
+let p2 = new Promise((reject, resolve) => {
+    fs.readFile('./b.txt', 'utf8', (err, data) => {
+        if(err)
+            reject(err)
+        resolve(data)
+    })
+})
+let p3 = new Promise((reject, resolve) => {
+    fs.readFile('./c.txt', 'utf8', (err, data) => {
         if(err)
             reject(err)
         resolve(data)
     })
 })
 
-promise.then((data) => {
-    console.log(data);
-}, (err) => {
-    console.log(err);
-})
+
+p1
+    .then((data) => {
+        console.log('A');
+        return p2
+    }, (err) => {
+        console.log(err);
+    })
+    .then((data) => {
+        console.log('B');
+        return p3
+    }, (err) => {
+        console.log(err);
+    })
+    .then((data) => {
+        console.log(data);
+    }, (err) => {
+        console.log(err);
+    })
